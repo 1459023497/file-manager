c121de6d6f92f90180a072f525781a07886de06b
Mr.Leon <80453008+1459023497@users.noreply.github.com>
2022-09-01 22:07:26 +0800

    打标签逻辑

--- src/gui/TagSellector.java
+++ src/gui/TagSellector.java
@@ -12,6 +12,7 @@ import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 public class TagSellector {
@@ -21,17 +22,20 @@ public class TagSellector {
     public TagSellector(File file, AddLabel addLabel){
         //窗口初始化
         JWindow window = new JWindow();
-        IPanel content = new IPanel(new Dimension(100,200));
+        IPanel content = new IPanel(new Dimension(200,300));
         window.setContentPane(content);
         window.pack();
-        window.setSize(100,150);
+        window.setSize(200,300);
+        window.setLocationRelativeTo(addLabel);
         window.setVisible(true);
         // TODO: 2022/9/1  窗口边框，背景，定位
         //顶部显示全部标签
-        IPanel top = new IPanel(new Dimension(100,50));
+        IPanel top = new IPanel(new Dimension(200,100));
+        top.setBorder(BorderFactory.createLineBorder(Color.red, 1, true));
         //底部显示选择的标签
-        IPanel down  = new IPanel(new Dimension(100,50));
-        down.setTags(new ArrayList<>());
+        IPanel down  = new IPanel(new Dimension(200,100));
+        down.setBorder(BorderFactory.createLineBorder(Color.red, 1, true));
+        down.setTags(new HashSet<>());
         //顶部查询加载全部标签
         conn = new JDBCConnector();
         tagService = new TagService(conn);
@@ -44,15 +48,30 @@ public class TagSellector {
             set.forEach(name -> top.add(new TagLabel(name, color.next(),down,2)));
         });
         //点击确定按钮后给文件添加标签
-        JButton button = new JButton("确定");
-        button.addActionListener(e -> {
+        JButton confirm = new JButton("确定");
+        confirm.addActionListener(e -> {
             //获取待添加的标签
-            ArrayList<String> tags = down.getTags();
+            HashSet<String> tags = down.getTags();
+            if(tags.isEmpty()) return;
             tags.forEach(t->{
+                //打标签
                 tagService.tag(t,file);
+                //System.out.print(t+"--");
             });
         });
+        //点击取消关闭选择窗口
+        JButton cancel = new JButton("取消");
+        cancel.addActionListener(e -> {
+            window.dispose();
+        });
 
+        //关闭数据库连接
+        conn.close();
+
+        window.add(top);
+        window.add(down);
+        window.add(confirm);
+        window.add(cancel);
 
     }
 }
